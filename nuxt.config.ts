a6185c1c3bf236a8953bc402af6490e054b22333
Jose Carrizales <jrafaelca@gmail.com>
2024-11-09 18:11:59 -0300

    feat: add multi lang (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
-  modules: ['@nuxt/ui']
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    future: {
+        compatibilityVersion: 4,
+    },
+    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+    i18n: {
+        locales: [{
+            code: 'en',
+            name: 'English',
+            file: 'en.json'
+        }, {
+            code: 'es',
+            name: 'Spanish',
+            file: 'es.json'
+        }],
+        lazy: true,
+        defaultLocale: process.env.APP_LOCALE || 'en',
+    }
 })
